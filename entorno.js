2a6d1a36ccd87912a39148a19540988f6f820046
Mauri Rodríguez <maurinightmare27@gmail.com>
2022-07-30 02:35:01 -0500

    first class

---
+++ entorno.js
@@ -0,0 +1,7 @@
+// \
+let NOMBRE = process.env.NOMBRE || 'sin nombre';
+let web = process.env.WEB || 'No tengo web';
+
+console.log('Hola '  + NOMBRE);
+
+console.log('mi web es ' + web)
---
+++ monohilo.js
@@ -0,0 +1,15 @@
+console.log("Hola mauri");
+
+
+let i = 0;
+
+setInterval(function(){
+    console.log(i);
+    i++;
+
+    // if(i === 5){
+    //     var a = 3 + Z;
+    // }
+},1000)
+
+console.log('segunda intruccion');
